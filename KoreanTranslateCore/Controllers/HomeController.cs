0f3ddc9c8d5c711280f4935d35b2a93b36ea0427
JayFromHatci <jaykang@hatci.com>
2025-03-10 15:07:04 -0400

    Update HomeController.cs

--- KoreanTranslateCore/Controllers/HomeController.cs
+++ KoreanTranslateCore/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Text.Encodings.Web;
+using System.Linq;
 
 namespace KoreanTranslateCore.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
+        {           
             return View();
         }
+        /*
+         * Create a method here to extract file directory and pass to TranslateController
+         * Maybe TranslateModel will handle the excel data?
+         */
     }
 }
